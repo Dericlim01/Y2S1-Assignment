d56e4e11c2c33ed2d49611bfca93d2a88fa34417
Dericlim01 <dericlimsengyik2004@gmail.com>
2024-07-17 21:13:21 +0800

    Login and create file
    Created Login page
    Created Login class
    Created create file class

---
+++ Login.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Login {
+    public String login(String n, String p) {
+        String line;
+
+        // Check file
+        Create_file file = new Create_file();
+        if (file.user_file()) {
+            // Read file
+            try (BufferedReader read = new BufferedReader(new FileReader("users.txt"))) {
+                while ((line = read.readLine()) != null) {
+                    // Create array and store data
+                    String[] data = line.split(",");
+                    String username = data[0];
+                    String password = data[1];
+
+                    // If username and password correct
+                    if (username.equals(n) && password.equals(p)) {
+                        return data[4];
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "";
+    }
+}
