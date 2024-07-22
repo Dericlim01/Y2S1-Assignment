bbcfb814fca8f3d8475e95fd266daf1328ae41ed
Dericlim01 <dericlimsengyik2004@gmail.com>
2024-07-22 23:16:25 +0800

    Fix bug
    Fix register page, update profile page and login page password of storing and reading

--- Register_Page.java
+++ Register_Page.java
@@ -31,75 +31,96 @@ public class Register_Page extends JFrame {
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
+        // Name Label
         JLabel name_lbl = new JLabel("Name : ");
         name_lbl.setBounds(30, 20, 50, 20);
         contentPane.add(name_lbl);
 
+        // Password Label
         JLabel pass_lbl = new JLabel("Password : ");
         pass_lbl.setBounds(30, 50, 100, 20);
         contentPane.add(pass_lbl);
 
+        // Contact Number Label
         JLabel cont_num_lbl = new JLabel("Contact Number : ");
         cont_num_lbl.setBounds(30, 80, 120, 20);
         contentPane.add(cont_num_lbl);
 
+        // Email Label
         JLabel email_lbl = new JLabel("Email : ");
         email_lbl.setBounds(30, 110, 100, 20);
         contentPane.add(email_lbl);
 
+        // Name Text Field
         JTextField name_txt_f = new JTextField();
         name_txt_f.setBounds(140, 20, 100, 20);
         contentPane.add(name_txt_f);
 
+        // Name status button
+        JLabel name_stat_lbl = new JLabel();
+        name_stat_lbl.setBounds(400, 290, 100, 20);
+        contentPane.add(name_stat_lbl);
+
+        // Password Text Field
         JPasswordField pass_txt_f = new JPasswordField();
         pass_txt_f.setBounds(140, 50, 100, 20);
         contentPane.add(pass_txt_f);
 
+        // Contact Number Text Field
         JTextField cont_num_txt_f = new JTextField();
         cont_num_txt_f.setBounds(140, 80, 100, 20);
         contentPane.add(cont_num_txt_f);
 
+        // Email Text Field
         JTextField email_txt_f = new JTextField();
         email_txt_f.setBounds(140, 110, 100, 20);
         contentPane.add(email_txt_f);
 
-        JButton register_btn = new JButton("Register");
+        // Register Button
+        JButton register_btn = new JButton("Sign Up");
         register_btn.setBounds(140, 140, 100, 20);
         register_btn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String name = name_txt_f.getText();
-                char[] pass = pass_txt_f.getPassword();
+                String pass = String.valueOf(pass_txt_f.getPassword());
                 String cont_num = cont_num_txt_f.getText();
                 String email = email_txt_f.getText();
                 String role = "customer";
                 Register register_user = new Register(role);
 
-                // User register successfuly
-                if (register_user.reg_user(name, pass, cont_num, email)) {
-                    // Show Message Dialog
-                    JOptionPane.showMessageDialog(
-                        null,
-                        "User Registered Successfully",
-                        "Information",
-                        JOptionPane.INFORMATION_MESSAGE);
-                } else {
-                    // User register failed, show option dialog
-                    int response = JOptionPane.showConfirmDialog(
-                        null,
-                        "Register Failed, Register again?",
-                        "Question",
-                        JOptionPane.YES_NO_OPTION);
-                    // Register again
-                    if (response == 1) {
-                        // Go to register page
-                        Register_Page reg = new Register_Page();
-                        reg.setVisible(true);
+                // Username available
+                if (register_user.chk_user(name)) {
+                    // User register successfuly
+                    if (register_user.reg_user(name, pass, cont_num, email)) {
+                        // Show Message Dialog
+                        JOptionPane.showMessageDialog(
+                            null,
+                            "User Registered Successfully",
+                            "Information",
+                            JOptionPane.INFORMATION_MESSAGE);
                     } else {
-                        // Stop registration, back to login
-                        Login_Page login = new Login_Page();
-                        login.setTitle("Login");
-                        login.setVisible(true);
+                        // User register failed, show option dialog
+                        int response = JOptionPane.showConfirmDialog(
+                            null,
+                            "Register Failed, Register again?",
+                            "Question",
+                            JOptionPane.YES_NO_OPTION);
+                        // Register again
+                        if (response == 1) {
+                            // Go to register page
+                            Register_Page reg = new Register_Page();
+                            reg.setVisible(true);
+                        } else {
+                            // Stop registration, back to login
+                            Login_Page login = new Login_Page();
+                            login.setTitle("Login");
+                            login.setVisible(true);
+                        }
                     }
+                } else {
+                    // Username exists
+                    name_stat_lbl.setText("Username already exists.");
+                    name_stat_lbl.setFont(new Font("Comic Sans MS", Font.PLAIN, 10));
                 }
             }
         });
